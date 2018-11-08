3d90596968750155ed25b48c5d1ccc7459baa2d9
Kitty <katherine.overman.2019@student.lhschools.org>
2018-11-08 10:23:52 -0500

    Coding Day 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main
+    public static void main (String[]args){
+        //declare
+        Scanner keyboard;
+        String A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z;
+        String _;
+        int vs;
+        String Computer;
+        String CPU;
+        String Com;
+        String Player;
+        String Human;
+
+
+
+        //initialization phase
+
+        keyboard = new Scanner(System.in);
+
+        System.out.println("Welcome to Hangman! Would you like to play against the computer or another player?");
+        vs = keyboard.nextLine();
+
+
+        ++If input is line 11-13, computer plays. If input is line 14-15, then second println++
+
+
+        System.out.println("Type a word or phrase. Make sure the other player doesn't see!");
+
+
+
+
+
+
+
+
+
+
+    }
 }
