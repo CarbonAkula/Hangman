9ff11490427a70b8b7d5136038927df712b2c19a
Kitty <katherine.overman.2019@student.lhschools.org>
2018-11-13 10:03:16 -0500

    Day 3

--- src/Main.java
+++ src/Main.java
@@ -20,8 +20,10 @@ public class Main {
         System.out.println("Welcome to Hangman! Would you like to play against the computer (Type C) or another player (Type P)?");
         vs = keyboard.nextLine();
 
-        System.out.println("Type a word or phrase. Make sure the other player doesn't see!");
+        if keyboard.nextLine() = p, System.println("Type a word or phrase. Make sure the other player doesn't see!");
         playerWord = keyboard.nextLine();
+
+        else System.println("Choose a category you would like a word or phrase from: Food(1), Movies and TV(2), Animals(3), Sports(4), Hobbies(5)");
 
 
     }
