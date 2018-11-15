d20ba193a710dc1d1d8ea73161d0130a9e971f58
Kitty <katherine.overman.2019@student.lhschools.org>
2018-11-15 12:37:28 -0500

    Auto stash before merge of "master" and "origin/master"

--- src/Main.java
+++ src/Main.java
@@ -22,9 +22,8 @@ public class Main {
 
         if keyboard.nextLine() = p, System.println("Type a word or phrase. Make sure the other player doesn't see!");
         playerWord = keyboard.nextLine();
+            hi
 
         else System.println("Choose a category you would like a word or phrase from: Food(1), Movies and TV(2), Animals(3), Sports(4), Hobbies(5)");
-
-
     }
 }
