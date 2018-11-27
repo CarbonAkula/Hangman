fce0892864ac3750491ca6295d069a2a738d8582
Kitty <katherine.overman.2019@student.lhschools.org>
2018-11-27 10:01:40 -0500

    Method

--- src/Main.java
+++ src/Main.java
@@ -11,18 +11,27 @@ public class Main {
         String c;
         String p;
         String playerWord;
+        String playerGuessWord;
+        String playerGuess;
+        String newPlayerGuessWord;
 
 
         //initialization phase
 
         keyboard = new Scanner(System.in);
 
+
         System.out.println("Welcome to Hangman! Would you like to play against the computer (Type C) or another player (Type P)?");
         vs = keyboard.nextLine();
 
         if keyboard.nextLine() = p, System.println("Type a word or phrase. Make sure the other player doesn't see!");
-        playerWord = keyboard.nextLine();
-            hi
+
+        playerWord = keyboard.nextLine().toLowerCase();
+
+        String[] playerWordBreakdown = playerWord.split("");
+        playerGuessWord = playerWord.substring(0,1)+playerGuess+playerWord.substring(1+1);
+        newPlayerGuessWord.contains("_");
+
 
         else System.println("Choose a category you would like a word or phrase from: Food(1), Movies and TV(2), Animals(3), Sports(4), Hobbies(5)");
     }
